4c252358979c9c033cc607f31a3c12106c1dfc24
ゆんふぃ <yunfie_tw@proton.me>
2026-06-11 23:12:11 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://yunfie-twitter.github.io",
-  base: "homepage",
+  base: "pages",
   integrations: [sitemap()],
 });
